039b5271147438d1c4e07320e3ec3db7b76a2787
Varga <vargazsigmond02@gmail.com>
2023-01-31 10:00:38 +0100

    vegpontok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Render,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { get } from 'http';
 import { Csavar } from './csavar.entity';
 import { AppService } from './app.service';
 
@@ -16,9 +23,22 @@ export class AppController {
   index() {
     return { message: 'Welcome to the homepage' };
   }
-  @Get('api/csavarwebshop')
+  @Get('api/csavar')
   async listCsavar() {
-    const webRepos = this.dataSource.getRepository(Csavar);
-    return await webRepos.find();
+    const aruhazRepo = this.dataSource.getRepository(Csavar);
+    return await aruhazRepo.find();
+  }
+
+  @Post('api/csavar')
+  newCsavar(@Body() csavar: Csavar) {
+    csavar.id = undefined;
+    const aruhazRepo = this.dataSource.getRepository(Csavar);
+    aruhazRepo.save(csavar);
+  }
+
+  @Delete('api/csavar/:id')
+  deleteCsavar(@Param('id') id: number) {
+    const aruhazRepo = this.dataSource.getRepository(Csavar);
+    aruhazRepo.delete(id);
   }
 }
